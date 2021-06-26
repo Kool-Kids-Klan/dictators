01bea1ce93f73a9d77085c54fd82d71d26728f90
492988 <492988@mail.muni.cz>
2021-06-26 22:28:27 +0200

    first real time django app

---
+++ dictators/dictators/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for dictators project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+import dictators.dictators_game.routing
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dictators.dictators.settings')
+
+# application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            dictators.dictators_game.routing.websocket_urlpatterns
+        )
+    ),
+})
