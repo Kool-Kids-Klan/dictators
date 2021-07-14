0781e6a1a53fabf6cefd0af0ae732591a60675d7
xkarnis <xkarnis@fi.muni.cz>
2021-07-14 19:59:38 +0200

    redirect

--- dictators-app/src/components/Pages/Auth/Login.tsx
+++ dictators-app/src/components/Pages/Auth/Login.tsx
@@ -1,15 +1,20 @@
-import Form from 'react-bootstrap/Form';
 import React, { FormEvent, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import sha256 from 'crypto-js/sha256';
+import { useHistory } from 'react-router-dom';
+import './Auth.css';
 import { useRecoilState } from 'recoil';
-import { usersState } from '../../../store/atoms';
-import './Login.css';
+import { appState, usersState } from '../../../store/atoms';
 import LoaderButton from '../../LoaderButton';
 
-const Register = () => {
+const Login = () => {
+  const history = useHistory();
+
   const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [users, setUsers] = useRecoilState(usersState);
+  const [, setAppState] = useRecoilState(appState);
+  const [users] = useRecoilState(usersState);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -19,12 +24,18 @@ const Register = () => {
     event.preventDefault();
 
     setIsLoading(true);
-    setUsers([...users, { email, password }]);
-    setIsLoading(false);
+    console.log(sha256(password));
+    if (users.some((user) => user.email === email && user.password === password)) {
+      setAppState({ authenticated: true });
+      history.push('/confirm');
+    } else {
+      alert('invalid');
+      setIsLoading(false);
+    }
   }
 
   return (
-    <div className="Register Login">
+    <div className="Login">
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="email">
           <Form.Label>Email</Form.Label>
@@ -44,11 +55,11 @@ const Register = () => {
           />
         </Form.Group>
         <LoaderButton block type="submit" isLoading={isLoading} disabled={!validateForm()}>
-          Sign up
+          Login
         </LoaderButton>
       </Form>
     </div>
   );
 };
 
-export default Register;
+export default Login;
