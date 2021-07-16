d330df373ed4966be5b8e397626e566b8a9cc33d
Daniel Timko <daniel.timko4@gmail.com>
2021-07-16 20:29:40 +0200

    user creation and authorization

--- dictators/dictators_game/consumers.py
+++ dictators/dictators_game/consumers.py
@@ -2,41 +2,34 @@ import json
 import asyncio
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
 
-from dictators.dictators_game.services.map_generator import generate_map
+from django.core import serializers
+
+from dictators.dictators_game.serializers import GameBoardSerializer
+from dictators.dictators_game.models import GameBoard
 
 
 class DictatorsConsumer(AsyncJsonWebsocketConsumer):
-    def __init__(self):
-        super(DictatorsConsumer, self).__init__()
-        print('init is called')
+    @database_sync_to_async
+    def db_tick(self, game_board):
+        return game_board.tick(addition=10)
 
-    async def tick(self, game_map):
+    async def tick(self, game_board):
         while True:
+            await self.db_tick(game_board)
             await self.send_json(content={'payload': {'message': 'hello', 'event': 'TICK'}})
-            print('this is happening')
             await asyncio.sleep(5)
 
-    @staticmethod
-    async def map_to_json(game_map):
-        map_dict = []
-        for row in game_map:
-            row_dict = []
-            for tile in row:
-                tile_dict = {
-                    'army': tile.army,
-                    'owner': tile.owner,
-                    'terrain': tile.terrain
-                }
-                row_dict.append(tile_dict)
-            map_dict.append(row_dict)
-
-        return map_dict
+    @database_sync_to_async
+    def create_game_board(self):
+        game_board = GameBoard.objects.create(game_board=[-1, -1, -1, -1, -1, -1, -1, -1, -1])
+        return game_board
 
     async def connect(self):
         print('User is trying to connect to room')
+        print('scope is', self.scope.keys())
 
-        print('this is scope', self.scope)
 
         self.room_name = self.scope['url_route']['kwargs']['room_code']
         self.room_group_name = f'room_{self.room_name}'
@@ -48,13 +41,13 @@ class DictatorsConsumer(AsyncJsonWebsocketConsumer):
         )
         await self.accept()
 
-        game_map = generate_map(16, 4, 8, 0)
-        # print('This is generated map', game_map)
-        # print(await self.map_to_json(game_map))
+        # serializer = GameBoardSerializer(data={'game_board': [-1, -1, -1, -1, -1, -1, -1, -1, -1]})
+        # serializer.is_valid(raise_exception=True)
+        # game_board = serializer.save()
+        game_board = await self.create_game_board()
 
-        await self.send_json(content={'payload': {'message': await self.map_to_json(game_map),
-                                                  'event': 'GAME_BOARD'}})
-        await self.tick(game_map)
+        await self.send_json(content={'payload': {'message': game_board.as_json(), 'event': 'GAME_BOARD'}})
+        await self.tick(game_board)
 
     async def disconnect(self, close_code):
         print("Disconnected")
