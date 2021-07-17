320121ae3832b763ee33734d20e2a73c69f7442b
Filip Karnis <karnisfilip@gmail.com>
2021-07-17 02:10:04 +0200

    Square.tsx logic moved to Game.tsx

--- dictators-app/src/components/Game/Square.tsx
+++ dictators-app/src/components/Game/Square.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { Coor, gameState, premovesState } from '../../store/atoms';
 
 interface ISquare {
   army?: number,
   owner?: string,
   terrain?: string,
-  coords: Coor
+  selected?: string,
+  select: () => void,
+  directions?: string,
 }
 
 const Square: React.FC<ISquare> = ({
-  army, owner, terrain, coords,
-}) => {
-  const [{ selected }, selectSquare] = useRecoilState(gameState);
-  const [premoves] = useRecoilState(premovesState);
-  const selectClass = (selected && coords[0] === selected[0] && coords[1] === selected[1]) ? 'selected' : '';
-  // TODO overwrites terrain in CSS
-  let directions = '';
-  premoves.forEach((premove) => {
-    if (premove.from[0] === coords[0] && premove.from[1] === coords[1]) {
-      directions += `${premove.direction} `;
-    }
-  });
-
-  return (
-    <td className={`game__tile ${owner} ${terrain} ${selectClass} ${directions}`} onClick={() => selectSquare({ selected: coords })}>{army}</td>
-  );
-};
+  army, owner, terrain, selected, select, directions,
+}) => (
+  <td className={`${owner} ${terrain} ${selected} ${directions}`} onClick={select}>{army}</td>
+);
 
 export default Square;
