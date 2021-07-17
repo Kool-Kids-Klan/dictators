eb012a93b8f704bed98d2e2b8ed4b79004e20c59
Daniel Timko <daniel.timko4@gmail.com>
2021-07-17 08:32:46 +0200

    bugfix

--- dictators/dictators_game/urls.py
+++ dictators/dictators_game/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from dictators.dictators_game import views
 
 urlpatterns = [
-    path("", views.MainPage.as_view()),
     path("api/user/create", views.CreateUser.as_view()),
     path("api/user/delete", views.DeleteUser.as_view()),
     path("api/user/authenticate", views.AuthenticateUser.as_view())
