50bc8cbf411f18005e22ecf5522a6963e6369a51
Filip Karnis <karnisfilip@gmail.com>
2021-07-20 03:29:06 +0200

    surrender

--- dictators-app/src/components/Game/GameMenu.tsx
+++ dictators-app/src/components/Game/GameMenu.tsx
@@ -3,13 +3,20 @@ import { ButtonGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { useRecoilValue } from 'recoil';
 import ExitButton from '../ExitButton';
-import { gameState } from '../../store/atoms';
+import { appState, gameState } from '../../store/atoms';
+import { currentGameSocket } from '../../store/selectors';
 
 const GameMenu = () => {
+  const { username } = useRecoilValue(appState);
   const { isOver, winner } = useRecoilValue(gameState);
+  const gameSocket = useRecoilValue(currentGameSocket);
 
   const surrender = () => {
-    // TODO
+    const data = {
+      event: 'SURRENDER',
+      message: username,
+    };
+    gameSocket.send(JSON.stringify(data));
   };
 
   const first = (isOver) ? (
