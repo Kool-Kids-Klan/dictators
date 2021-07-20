3256ae4e715e2f183de8a2af05f1e3df3fe03011
xbaran4 <pbaran@redhat.com>
2021-07-20 04:17:46 +0200

    leaderboard styling

--- dictators-app/src/components/Pages/LeaderboardItem.tsx
+++ dictators-app/src/components/Pages/LeaderboardItem.tsx
@@ -4,13 +4,26 @@ interface ILeaderboardItemProps {
   username: string,
   games_won: number,
   games_played: number,
+  index: number,
 }
 
-const LeaderboardItem = ({ username, games_won, games_played }: ILeaderboardItemProps) => (
+const LeaderboardItem = ({
+  username, games_won, games_played, index,
+}: ILeaderboardItemProps) => (
   <div className="leaderboard__item">
-    <span className="leaderboard__user">{username}</span>
-    <span className="leaderboard__games-won">{games_won}</span>
-    <span className="leaderboard__games-played">{games_played}</span>
+    <span className="leaderboard__user">
+      {`${index + 1}. place: ${username}`}
+    </span>
+    <span className="leaderboard__games-won">
+      Won:
+      {' '}
+      {games_won}
+    </span>
+    <span className="leaderboard__games-played">
+      Played:
+      {' '}
+      {games_played}
+    </span>
   </div>
 );
 
--- dictators-app/src/utils/endpoints.ts
+++ dictators-app/src/utils/endpoints.ts
@@ -1,7 +1,7 @@
 // export const ORIGIN_URL = 'http://localhost:3000';
 const apiPrefix = `${window.location.hostname}:8000`;
 export const CREATE_USER_URL = `http://${apiPrefix}/api/user/create`;
-export const LEADERBOARD_URL = `http://${apiPrefix}/api/userboard`;
+export const LEADERBOARD_URL = `http://${apiPrefix}/api/leaderboard`;
 export const AUTH_USER_URL = `http://${apiPrefix}/api/user/authenticate`;
 
 export const reqOptions = (body: any) => ({
