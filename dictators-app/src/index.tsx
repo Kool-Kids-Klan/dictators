c0f44d6933ec8fe128d3b9c8c638ba0e5914f10a
Filip Karnis <karnisfilip@gmail.com>
2021-08-27 20:23:10 +0200

    github pages

--- dictators-app/src/index.tsx
+++ dictators-app/src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Dictators">
       <RecoilRoot>
         <App />
       </RecoilRoot>
--- dictators-app/src/utils/endpoints.ts
+++ dictators-app/src/utils/endpoints.ts
@@ -1,14 +1,17 @@
-// export const ORIGIN_URL = 'http://localhost:3000';
-const apiPrefix = `${window.location.hostname}:8000`;
-export const CREATE_USER_URL = `http://${apiPrefix}/api/user/create`;
-export const LEADERBOARD_URL = `http://${apiPrefix}/api/leaderboard`;
-export const AUTH_USER_URL = `http://${apiPrefix}/api/user/authenticate`;
+export const ORIGIN_URL = 'https://kool-kids-klan.github.io/';
+export const BACKEND_URL = 'dictators-django.herokuapp.com';
+// TODO config file
+// export const ORIGIN_URL = 'http://localhost';
+// export const BACKEND_URL = `${window.location.hostname}:8000`;
+export const CREATE_USER_URL = `https://${BACKEND_URL}/api/user/create`;
+export const LEADERBOARD_URL = `https://${BACKEND_URL}/api/leaderboard`;
+export const AUTH_USER_URL = `https://${BACKEND_URL}/api/user/authenticate`;
 
 export const reqOptions = (body: any) => ({
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    Origin: window.location.host,
+    Origin: ORIGIN_URL,
   },
   body: JSON.stringify(body),
 });
